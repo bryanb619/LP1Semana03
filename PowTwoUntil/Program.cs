9d3107dbc11586cf5ea11ba1a26865ddfc975ce1
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-06 23:34:03 +0000

    Added First Void!

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -6,12 +6,22 @@ namespace PowTwoUntil
     {
         private static void Main(string[] args)
         {
-            for (int i= 0; i<= (1<<5); i = i<< 1)
+            // Calling void twice :(
+            PowersOf2Until5();
+            PowersOf2Until5(); 
+        }
+
+        //
+        private static void PowersOf2Until5()
+        {
+            
+            for (int i= 1; i<= (1<<5); i = i<< 1)
             {
-                Console.WriteLine("Hello, World!");
+                Console.WriteLine(i);
 
             }
  
+
         }
     }
 }
