a3cb70d7827efe1ba3802e865076df1e7af1c9e4
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-06 23:31:35 +0000

    Added PowTwoUntil to solution

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            for (int i= 0; i<= (1<<5); i = i<< 1)
+            {
+                Console.WriteLine("Hello, World!");
+
+            }
+ 
+        }
+    }
+}
