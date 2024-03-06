2459aa4a93b64727f9585c5df93c7fbc68b44cf7
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-06 22:38:47 +0000

    Added simple foreach with incrementation

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -4,15 +4,31 @@ namespace RightSize
 {
     class Program
     {
+
+        /// <summary>
+        ///  Basic programs, 
+        /// </summary>
+        /// <param name="args">Not used</param>
         private static void Main(string[] args)
         {
-            // iterate each position in the string args
-            foreach(char c in args)
+
+            // print args with lenght > 3
+            if(args.Length > 3)
             {
+
+                foreach(string s in args)
+                {
+                    int i = 0;  
+
+                    Console.WriteLine(); 
+
+                    i++; 
+
+                }
                 
-
-
             }
+
+
         }
     }
 }
