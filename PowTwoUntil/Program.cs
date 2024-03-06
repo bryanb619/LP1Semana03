11b3775154549ef4588257c9f28dbd1dd6e10f3a
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-06 23:36:54 +0000

    Added comments!

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -11,12 +11,17 @@ namespace PowTwoUntil
             PowersOf2Until5(); 
         }
 
-        //
+        // new method
+        /// <summary>
+        ///  This method writes the number 1 to 32
+        ///  the for shifts i << 1, so first 1 ... 32
+        /// </summary>
         private static void PowersOf2Until5()
         {
-            
+
             for (int i= 1; i<= (1<<5); i = i<< 1)
             {
+                // writes a decimal value
                 Console.WriteLine(i);
 
             }
