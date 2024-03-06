222fe3bfa7933b80af6ad71f28207d71e0325bad
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-06 22:54:39 +0000

    Added Main XML & Comments

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,32 +6,42 @@ namespace RightSize
     {
 
         /// <summary>
-        ///  Basic programs, 
+        ///  Basic programs, iterates over arguments passed in console
+        /// 
+        ///  1. backup variable -> used in for loop
+        ///  2. foreach -> checks each string in args array
+        ///  3. continue if argument size is bigger then 3
+        ///  4. STOP CODE when argument > 6 and return (STOP CODE)
+        ///         -> return position not ideal
+        ///  5. print only args(strings) with Length > 3
+        ///  6. increment i always when loop reaches iteration end
         /// </summary>
         /// <param name="args">Not used</param>
         private static void Main(string[] args)
         {
 
+            // 1.
             int i = 0;
 
-            
+            // 2. iterate over strings in args -> []
             foreach(string s in args)
             {
-
-               
-                // print args with lenght > 3
+                
+                // 3. check if argument size is bigger then 3
                 if (s.Length > 3)
                 {
 
+                    // 4. check if has more than 6 elements
                     if(s.Length > 6)
                     {
                         return; 
                     }
 
+                    // 5.  
                     Console.WriteLine(args[i]);
                 } 
 
-                
+                // 6. increment i
                 i++;
                 
             }
