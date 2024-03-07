5d315e11bd0e24944229c9828925c056cacd4d06
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-07 21:05:17 +0000

    Added switch case for habilities

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -6,12 +6,45 @@ namespace HeroPerks
     {
         private static void Main(string[] args)
         {
+
             Perks perks; 
+
 
            foreach(string s in args)
            {
 
+                 
+
+
            }
+
+            // 
+           perks = Perks.WaterBreathing;
+
+            switch(perks)
+            {
+                case Perks.WaterBreathing:
+                {
+
+                    break; 
+                }
+
+                case Perks.Stealth:
+                {
+                    break; 
+                }
+
+                case Perks.AutoHeal:
+                {
+                    break; 
+                }
+
+                case Perks.DoubleJump:
+                {
+                    break; 
+                }
+                
+            }
         }
     }
 }
