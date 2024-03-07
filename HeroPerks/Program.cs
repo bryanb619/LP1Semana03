0c547b6c6fc8e2dc34a09b56bf14599dc5f60158
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-07 22:10:37 +0000

    Added new logic
    Logic: Convert every perk enum in a string with foreach

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -6,45 +6,63 @@ namespace HeroPerks
     {
         private static void Main(string[] args)
         {
+            //Perks perks;
 
-            Perks perks; 
+            //Perks perk;
 
+            int i = 0; 
 
-           foreach(string s in args)
-           {
-
-                 
-
-
-           }
-
+            string[] perksArray = new string[Enum.GetNames(typeof(Perks)).Length]; 
+                  
             // 
-           perks = Perks.WaterBreathing;
+           //perks = Perks.WaterBreathing;
 
-            switch(perks)
+            foreach(string perkName in Enum.GetNames(typeof(Perks)))
             {
-                case Perks.WaterBreathing:
-                {
 
-                    break; 
-                }
+                perksArray[i] = perkName;
+                Console.WriteLine(perksArray[i]); 
 
-                case Perks.Stealth:
-                {
-                    break; 
-                }
+                i++; 
 
-                case Perks.AutoHeal:
-                {
-                    break; 
-                }
-
-                case Perks.DoubleJump:
-                {
-                    break; 
-                }
+                
                 
             }
+
+             
+
+        /*
+
+            foreach(string s in args)
+            {
+            /*
+                switch(perks)
+                {
+                    case Perks.WaterBreathing:
+                    {
+
+                        break; 
+                    }
+
+                    case Perks.Stealth:
+                    {
+                        break; 
+                    }
+
+                    case Perks.AutoHeal:
+                    {
+                        break; 
+                    }
+
+                    case Perks.DoubleJump:
+                    {
+                        break; 
+                    }
+                } */
         }
+
+            //perk = (Perks)Enum.Parse(typeof(Perks), s); 
+
+        
     }
 }
