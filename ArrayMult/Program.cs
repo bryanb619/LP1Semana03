160b94293bb8754fdf958d6a3f193d9aafc6d2d9
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-08 13:17:40 +0000

    Added arrays and fors logic

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -1,11 +1,30 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ArrayMult
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+
+            // arrays 
+            float[,] a = new float[2, 2];
+            float[,] b = new float[2, 1];
+
+            for(int i = 0; i < 2; i++)
+            {
+                for(int j = 0; j < 2 ; j++)
+                {
+
+                    
+
+
+                }
+            }
+
+
+
             Console.WriteLine("Hello, World!");
         }
     }
