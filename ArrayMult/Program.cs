d3de53c0ff5a4ecca36acbb4c409437af69db4b0
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-08 13:40:24 +0000

    Fixed loop calculation logic

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -12,37 +12,33 @@ namespace ArrayMult
             float[,] a = new float[2, 2];
             float[,] b = new float[2, 1];
 
-            float CalcResult = 0F; 
+           
 
 
-            
+            // A array values
             a[0,0] = float.Parse(args[0]);
             a[0,1] = float.Parse(args[1]);
-
 ;           a[1,0] = float.Parse(args[2]);
             a[1,1] = float.Parse(args[3]);
 
-
-
-            for (int x = 4; x < args.Length; x++)
-            {
-                b[x,0] = float.Parse(args[x]);
-            }
-   
-           
-    
+            // B array values
+            b[0,0] = float.Parse(args[4]);
+            b[1,0] = float.Parse(args[5]);   
         
+            
             for(int i = 0; i < 2; i++)
             {
+                float CalcResult = 0F; 
                 for(int j = 0; j < 2 ; j++)
                 {
-                    
+                    // Multiplication
                     CalcResult += a[i, j] * b[j, 0];
                     
                 }
+                Console.WriteLine(CalcResult);
             }
 
-            Console.WriteLine(CalcResult);
+           
         }
     }
 }
