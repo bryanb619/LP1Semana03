b2f2e9d23c28723c1499f84bf8e4b2780a9f67b1
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-08 14:40:26 +0000

    unfinished work in PlayerPowers

--- PlayerPowers/Program.cs
+++ PlayerPowers/Program.cs
@@ -7,12 +7,10 @@ namespace PlayerPowers
         private static void Main(string[] args)
         {
 
-            // array
-            //string[] powers = new string[] {};
-
 
             // request input from user (int)
             Console.WriteLine("Enter a number: ");
+
             // convert to int
             int n = int.Parse(Console.ReadLine()); 
 
@@ -20,21 +18,61 @@ namespace PlayerPowers
             string[][] players = new string[n][3];
 
 
+            Powers power; 
+
+
             for(int i = 0; i < players.Length; i++)
             {
                 // Request input from user (string)
                 for(int j= 0; j < 3; j++)
                 {
-                    Console.WriteLine("Enter a power: ");
+
+                    Console.WriteLine($"Player {i} enter a power: ");
+
                     string pow = Console.ReadLine();
+                    
+                    p_power = (Powers)Enum.Parse(typeof(Powers), pow);
 
-                    // add to array
-                    players[i][j] = pow;
+                    switch(p_power)
+                    {
+
+                        case Powers.Fly:
+
+                            players[i][j] ^= powers.Fly;
+                            break;
+
+                        case Powers.XRayVision:
+               
+                            players[i][j] ^= "XRayVision";
+                            break;
+
+                        case Power.SuperStrength:
+                
+                            players[i][j] ^= "SuperStrength";
+                            break;
+
+
+                        default:
+                            Console.WriteLine("Invalid Power");
+                            break;
+                    }
+                 
                 }
+                Console.WriteLine(players[i][j]);
+            }
+/*
 
-
+            for(int i = 0; i < players.Length; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    if(players[i] 
+                    )
+                    Console.WriteLine(players[i][j]);
+                }
             }
 
-        }
+        }~
+        */
     }
 }
