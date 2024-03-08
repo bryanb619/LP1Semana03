ac0508eb5eebee6f27451e04c8232f734158483c
Steven Hall <stevenbryanb.work@gmail.com>
2024-03-08 13:24:49 +0000

    Added inside for logic

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -12,20 +12,27 @@ namespace ArrayMult
             float[,] a = new float[2, 2];
             float[,] b = new float[2, 1];
 
+            float CalcResult = 0F; 
+
+            foreach (string arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+    
+        
             for(int i = 0; i < 2; i++)
             {
                 for(int j = 0; j < 2 ; j++)
                 {
-
                     
-
-
+                    CalcResult += a[i, j] * b[j, 0];
+                    
                 }
             }
 
 
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(CalcResult);
         }
     }
 }
